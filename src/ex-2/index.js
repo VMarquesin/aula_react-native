5f85c9df56d7be7498e5342fe9ecce28916aaa4e
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-02-28 22:42:35 -0300

    aula 28/02
    criei exemplo dois

---
+++ src/ex-2/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function mensagem () {
+    return(
+        <View>
+            <Text>Hello React-Native</Text>
+        </View>
+    );
+}
