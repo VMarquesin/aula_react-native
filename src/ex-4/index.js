971dc720627c54a8253f06e7900740758381548a
VINICIUS RODRIGUES MARQUESIN <vinicius.marquesin>
2024-04-08 20:39:03 -0300

    exercio-4

--- src/ex-4/index.js
+++ src/ex-4/index.js
@@ -1,47 +1,101 @@
+// import { useState } from 'react';
+
+// import { View, Text, TextInput, Pressable } from 'react-native';
+
+// import styles from './styles';
+
+// export default function Exemplo4(){
+
+//     const [txtDigitado, setTxtDigitado] = useState('Texto digitado');
+//     const [txt2, setTxt2] = useState('');
+//     const [txtTela, setTxtTela] = useState('');
+
+//     function atualizaTextoHandle (txt) {
+//         setTxtDigitado(txt);
+//         setTxt2 (txt2)
+//         setTxtTela(txt2);
+//     }
+
+//     return(
+//         <View style={styles.container}>
+//             <Text style={styles.titulo}>Exemplo 4</Text>
+
+//             <Text style={styles.texto}>{txtDigitado}</Text>
+
+//             <Text style={styles.texto}>Nome</Text>
+            
+//             <TextInput 
+//             style={styles.input}
+//             onChangeText={(valor) => atualizaTextoHandle(valor)}
+//             />
+
+//             {/* <View style={styles.linha}></View> */}
+
+//             <Text style={styles.texto}>Sobrenome</Text>
+
+//             <Text style={styles.texto}>{txtTela}</Text>
+
+//             <TextInput 
+//                 style={styles.input}
+//                 onChangeText={(vlr) => setTxt2(vlr)}
+//                 rvalue={txt2}
+//             />
+
+//             <Pressable style={({ pressed }) => pressed ? [styles.botao, styles.botaoPress] : styles.botao}>
+//                 <text style={styles.txtBotao}>Exibir texto</text>
+//             </Pressable>
+
+
+//         </View>
+//     );
+// }
+import { View, Text, TextInput, Pressable } from "react-native";
+
 import { useState } from 'react';
 
-import { View, Text, TextInput, Pressable } from 'react-native';
+import styles from "./styles";
 
-import styles from './styles';
+export default function atividade_4 () {
 
-export default function Exemplo4(){
-
-    const [txtDigitado, setTxtDigitado] = useState('Texto digitado');
+    const [txtDigitado, setTxtDigitado] = useState('Insira seu NOME e SOBRENOME');
     const [txt2, setTxt2] = useState('');
     const [txtTela, setTxtTela] = useState('');
 
-    function atualizaTextoHandle (txt) {
+    function ResultadoNome (txt) {
         setTxtDigitado(txt);
+        setTxtTela(txtTela)
+        setTxt2(txt2);
     }
 
     return(
         <View style={styles.container}>
-            <Text style={styles.titulo}>Exemplo 4</Text>
 
-            <Text style={styles.texto}>{txtDigitado}</Text>
+        <Text style={styles.titulo}>Atividade 4</Text>
 
-            <TextInput 
-            style={styles.input}
-            onChangeText={(valor) => atualizaTextoHandle(valor)}
-            />
+        <Text style={styles.texto}>{txtDigitado}</Text>
 
-            <View style={styles.linha}></View>
+        <Text style={styles.texto}>Nome :</Text>
 
-            
+        <TextInput
+        style={styles.input}
+        onChangeText={(vlr) => setTxtTela(vlr)}
+        value={txtTela}
+        />
 
-            <Text style={styles.texto}>{setTxtTela}</Text>
+        <Text style={styles.texto}>Sobrenome :</Text>
 
-            <TextInput 
-                style={styles.input}
-                onChangeText={(vlr) => setTxt2(vlr)}
-                rvalue={txt2}
-            />
+        <TextInput
+        style={styles.input}
+        onChangeText={(vlr) => setTxt2(vlr)}
+        value={txt2}
+        />
 
-            <Pressable style={({ pressed }) => pressed ? [styles.botao, styles.botaoPress] : styles.botao}>
-                <text style={styles.txtBotao}>Exibir texto</text>
-            </Pressable>
-
-
-        </View>
+        <Pressable
+        style={({ pressed }) => (pressed ? [styles.botao, styles.botaoPress] : styles.botao)}
+        onPress={() => ResultadoNome(`${txtTela} ${txt2}`)(setTxt2(''), setTxtTela(''))}
+        >
+        <Text style={styles.txtBotao}>Exibir texto</Text>
+        </Pressable>
+    </View>
     );
 }
