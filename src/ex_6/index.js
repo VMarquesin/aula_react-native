8a323ecedaf65676b4967746728333cd2cf6872b
VINICIUS RODRIGUES MARQUESIN <vinicius.marquesin>
2024-06-10 19:56:05 -0300

    exemplo 7

--- src/ex_6/index.js
+++ src/ex_6/index.js
@@ -13,43 +13,27 @@ export default function Exemplo6 () {
     const [altura, setAltura] = useState();
     const [imc, setImc] = useState(0.00);
     const mensErro = 'Valores inseridos de forma incorreta!';
-    const [abaixo, setAbaixo] = useState();
+    const [mensagem, setMensagem] = useState();
 
     function calcularImc () {
         const tmpImc = peso/(altura*altura);
-        setImc (tmpImc) 
+        setImc (tmpImc) ;
         if(tmpImc < 18.5){
-            setAbaixo ('abaixo do peso')
+            setMensagem ('abaixo do peso');
         } else if (tmpImc > 18.5 && tmpImc < 24.9){
-            setAbaixo ('Peso normal')
+            setMensagem ('Peso normal');
         } else if (tmpImc > 25 && tmpImc < 29.9){
-            setAbaixo('Sobrepeso')
+            setMensagem('Sobrepeso');
         } else if (tmpImc > 30 && tmpImc < 34.9){
-            setAbaixo('Obesidade grau 1')
+            setMensagem('Obesidade grau 1');
         } else if (tmpImc > 35 && tmpImc <39.9){
-            setAbaixo('Obesidade grau 2')
-        } else if (tmpImc > 40){
-            setAbaixo('Obesidade grau 3')
-        };
-   
-        // if(tmpImc < 18.5){
-        //     setAbaixo ('abaixo do peso')
-        // };
-   
-        // if(tmpImc < 18.5){
-        //     setAbaixo ('abaixo do peso')
-        // };
-   
-        // if(tmpImc < 18.5){
-        //     setAbaixo ('abaixo do peso')
-        // };
-   
+            setMensagem('Obesidade grau 2');
+        } else {
+            setMensagem('Obesidade grau 3');
+        }; 
    
     }
 
-    // function abaixoPeso(){
-    //     abaixo (calcularImc < 18.5) ('Abaixo do peso');
-    // }
 
     return(
         <View style={styles.container}>
@@ -61,7 +45,7 @@ export default function Exemplo6 () {
 
             <Text style={styles.imc}>{isNaN(imc) ? mensErro : imc.toFixed(2)}</Text>
 
-            <text style={styles.imc}> {abaixo} </text>
+            <text style={styles.imc}> {mensagem} </text>
             
             <Botao calcular={calcularImc}>Calcular</Botao>
            
