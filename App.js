4088a59b6254f434752e054445f7b91038c78af7
VINICIUS RODRIGUES MARQUESIN <vinicius.marquesin>
2024-06-17 21:20:51 -0300

    atiivdade 8

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
 // import Mensagem from './src/ex-2';
 // import Atividade1 from './src/ativ-1';
@@ -9,23 +9,24 @@ import { StyleSheet, View } from 'react-native';
 // import Exemplo_5 from './src/ex-5';
 // import Atividade_5 from './src/atv-5';
 // import Exemplo6 from './src/ex_6';
-import Exemplo7 from './src/ex_7';
+// import Exemplo7 from './src/ex_7';
+import Exemplo8 from "./src/ex8";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Exemplo7 />
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <View style={styles.container}>
+         <Exemplo8 />
+         <StatusBar style="auto" />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#C1C1C1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 18,
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#C1C1C1",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 18,
+   },
 });
